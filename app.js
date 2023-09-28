d7c24e6f946efc9b8e28529e5bfd0feeccfa9480
ryohei8 <ryohei.chihara.web@gmail.com>
2023-09-29 01:01:02 +0900

    [add] 使い機能実装

---
+++ app.js
@@ -0,0 +1,6 @@
+$("#addTodo").on("click", function() {
+    const inputTodo = $("#input").val();
+    $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
+
+    $("#input").val("");
+});
